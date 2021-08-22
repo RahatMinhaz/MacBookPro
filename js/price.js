3af99ab168bbeae67d116a9e9135c532eeb6b8e7
RahatMinhaz <minhaz7591@gmail.com>
2021-08-22 22:17:21 +0600

    comments added

--- js/price.js
+++ js/price.js
@@ -2,6 +2,8 @@ const basePrice = document.getElementById("base-price");
 const getGrandTotal = document.getElementById("total");
 const getBigTotal = document.getElementById("big-total");
 var couponTotal = 0;
+
+// Main Function
 function updateTotal(){
     const memoryCharge = parseFloat(memoryValue.innerText);
     const productPrice = parseFloat(basePrice.innerText);
@@ -9,10 +11,13 @@ function updateTotal(){
     const deliveryCharge = parseFloat(shippingValue.innerText);
     const grandTotal = memoryCharge + storageCharge + deliveryCharge + productPrice;
     getGrandTotal.innerText = grandTotal;
+    // Bonus Part 1 (Showing the total below)
     getBigTotal.innerText = grandTotal; 
+    // Bonus Part-1 Ends
     couponTotal = 1 * grandTotal;
 }
 
+// Memory Options
 const btn1 = document.getElementById("first-memory-value");
 const btn2 = document.getElementById("second-memory-value");
 const memoryValue = document.getElementById("memory-price");
@@ -24,6 +29,8 @@ btn2.addEventListener("click", function(){
     memoryValue.innerText = "180";
     updateTotal();
 })
+
+// Storage Options
 const btn3 = document.getElementById("first-storage-value");
 const btn4 = document.getElementById("second-storage-value");
 const btn5 = document.getElementById("third-storage-value");
@@ -40,6 +47,8 @@ btn5.addEventListener("click", function(){
     storageValue.innerText = "180";
     updateTotal();
 })
+
+// Delivery Options
 const btn6 = document.getElementById("first-shipping-value");
 const btn7 = document.getElementById("second-shipping-value");
 const shippingValue = document.getElementById("delivery-price");
@@ -52,7 +61,7 @@ btn7.addEventListener("click", function(){
     updateTotal();
 })
 
-
+// Bonus Part-2 (Coupon Code)
 const btn8 = document.getElementById("coupon-input");
 btn8.addEventListener("click", function(){
     const couponField = document.getElementById("coupon-code");
@@ -62,5 +71,6 @@ btn8.addEventListener("click", function(){
         getGrandTotal.innerText = couponTotal * 0.8;
         getBigTotal.innerText = couponTotal * 0.8;
     }
+    // Clears the input
     couponField.value = "";
 })
